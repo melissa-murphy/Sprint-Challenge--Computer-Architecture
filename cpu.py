558caebf991409919491773e0f69b9d50d498761
melissa-murphy <melissamurphy.ca@gmail.com>
2020-05-25 11:19:36 -0700

    functional but needs refactoring

--- cpu.py
+++ cpu.py
@@ -2,48 +2,17 @@
 
 import sys
 
-AND = 0b10101000  # ALU command
-CMP = 0b10100111  # ALU command
-HLT = 0b00000001
-JEQ = 0b01010101
-JMP = 0b01010100
-JNE = 0b01010110
-LDI = 0b10000010
-MUL = 0b10100010  # ALU command
-NOT = 0b01101001  # ALU command
-OR = 0b10101010  # ALU command
-POP = 0b01000110
-PRN = 0b01000111
-PUSH = 0b01000101
-SHL = 0b10101100  # ALU command
-SHR = 0b10101101  # ALU command
-XOR = 0b10101011  # ALU command
-
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
-        self.PC = 0  # starting program counter
-        self.SP = 0xF4  # starting pointer stored
-        self.FL = 0b00000000  # starting flag
-
-        # handle ls8 operations
-        self.cmd_op = {
-            0b10100111: self.cmp_func,  # 167
-            0b00000001: self.hlt,  # 1
-            0b01010101: self.jeq,  # 85
-            0b01010100: self.jmp,  # 84
-            0b01010110: self.jne,  # 86
-            0b10000010: self.ldi,  # 130
-            0b10100010: self.mul,  # 162
-            0b01000110: self.pop,  # 70
-            0b01000111: self.prn,  # 71
-            0b01000101: self.push  # 69
-        }
+        self.sp = 0xF4
+        self.fl = 0b00000000
 
     def ram_read(self, address):
         return self.ram[address]
@@ -51,65 +20,8 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
-    def hlt(self, operand_a, operand_b):  # halt program and exit
-        return (0, False)
-
-    def ldi(self, operand_a, operand_b):  # assign value to register
-        self.reg[operand_a] = operand_b
-        return (3, True)
-
-    def prn(self, operand_a, operand_b):  # print value in current register
-        print(self.reg[operand_a])
-        return (2, True)
-
-    # multiply values from both registers and store in first register
-    def mul(self, operand_a, operand_b):
-        self.alu("MUL", operand_a, operand_b)
-        return (3, True)
-
-    def pop(self, operand_a, operand_b):
-        # read starting point from stack pointer variable
-        value = self.ram_read(self.SP)
-        self.reg[operand_a] = value  # write to indicated register
-        self.SP += 1  # increment stack pointer to next occupied memory in the stack
-
-        return (2, True)
-
-    def push(self, operand_a, operand_b):
-        self.SP -= 1  # decrement stack point and return open spot in memory
-        # gets value from operand_a current register
-        value = self.reg[operand_a]
-        # writes to above value to the stack pointer address
-        self.ram_write(value, self.SP)
-
-        return (2, True)
-
-    def cmp_func(self, operand_a, operand_b):
-        self.alu("CMP", operand_a, operand_b)
-        return (3, True)
-
-    def jmp(self, operand_a, operand_b):
-        # retrieve address from reg and set to PC
-        self.PC = self.reg[operand_a]
-        return (0, True)
-
-    def jeq(self, operand_a, operand_b):
-        # if flag is True (1) set pc to address retreived from reg
-        if self.FL == 0b00000001:
-            self.PC = self.reg[operand_a]
-            return (0, True)
-        return (2, True)
-
-    def jne(self, operand_a, operand_b):
-        # if flag is False (0) set pc to address retreived from reg
-        if self.FL != 0b00000001:
-            self.PC = self.reg[operand_a]
-            return (0, True)
-        return (2, True)
-
     def load(self):
         """Load a program into memory."""
-        # print("loading....")
         program = sys.argv[1]
         address = 0
 
@@ -118,38 +30,47 @@ class CPU:
                 count = 0
                 for line in f:
                     count += 1
-                    # find and ignore anything following #
+                    # Process comments, and ignore them.
+                    # Ignore anything after a # symbol
                     comment_split = line.split('#')
-                    number = comment_split[0].strip()  # convert binary to int
 
+                    # Convert numbers from binary strings to integers
+                    num = comment_split[0].strip()
                     try:
-                        x = int(number, 2)
+                        x = int(num, 2)
                     except ValueError:
                         continue
+                    # print(f"{x:08b}: {x:d}")
 
                     self.ram[address] = x
-
                     address += 1
 
         except FileNotFoundError:
-            print(f"{sys.argv[0]}: {program} not found.")
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found.")
             sys.exit(2)
 
-    def alu(self, op, reg_a, reg_b):
+    def alu(self, op, reg_a, reg_b=None):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
         elif op == "MUL":
-            self.reg[reg_a] = (self.reg[reg_a]) * (self.reg[reg_b])
-        elif op == "CMP":
-            if self.reg[reg_a] > self.reg[reg_b]:
-                self.FL = 0b00000010  # 2
-            elif self.reg[reg_a] < self.reg[reg_b]:
-                self.FL = 0b00000100  # 4
-            elif self.reg[reg_a] == self.reg[reg_b]:
-                self.FL = 0b00000001  # 1
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+        elif op == "MOD":
+            self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
+        elif op == "AND":
+            self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+        elif op == "OR":
+            self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
+        elif op == "XOR":
+            self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
+        elif op == "NOT":
+            self.reg[reg_a] = ~self.reg[reg_a]
+        elif op == "SHL":
+            self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
+        elif op == "SHR":
+            self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -160,12 +81,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.PC,
+            self.pc,
             # self.fl,
             # self.ie,
-            self.ram_read(self.PC),
-            self.ram_read(self.PC + 1),
-            self.ram_read(self.PC + 2)
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
@@ -176,21 +97,137 @@ class CPU:
     def run(self):
         """Run the CPU."""
         running = True
-        # refactored to use pointer and operandi
-
         while running is True:
-            IR = self.ram[self.PC]
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # self.trace()
 
-            operand_a = self.ram_read(self.PC + 1)
-            operand_b = self.ram_read(self.PC + 2)
-            self.trace()
+            # HLT or HALT! -- Halt the CPU (and exit the emulator).
+            if IR == 0b00000001:
+                running = False
 
-            try:
-                op_output: self.cmd_op[IR](operand_a, operand_b)
+            # LDI register immediate -- Set the value of a register to an integer.
+            elif IR == 0b10000010:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
 
-                running = op_output[1]
-                self.PC += op_output[0]
+            # PRN register pseudo-instruction -- Print numeric value stored in the given register.
+            elif IR == 0b01000111:
+                print(self.reg[operand_a])
+                self.pc += 2
 
-            except:
-                print(f"Unknown Command: {IR}")
-                sys.exit(1)
+            # stack op
+
+            # PUSH register -- Push the value in the given register on the stack.
+            elif IR == 0b01000101:
+                # Decrement the SP
+                # Copy the value in the given register to the address pointed to by SP
+                self.sp -= 1
+                self.ram[self.sp] = self.reg[operand_a]
+                self.pc += 2
+
+            # POP register -- Pop the value at the top of the stack into the given register.
+            elif IR == 0b01000110:
+                # Copy the value from the address pointed to by SP to the given register.
+                # Increment SP.
+                self.reg[operand_a] = self.ram[self.sp]
+                self.sp += 1
+                self.pc += 2
+
+            # subroutine
+
+            # CALL register - Calls a subroutine (function) at the address stored in the register.
+            elif IR == 0b01010000:
+                self.sp -= 1
+                self.ram[self.sp] = self.pc + 2
+                self.pc = self.reg[operand_a]
+
+            # RET - Return from subroutine.
+            elif IR == 0b00010001:
+                # Pop the value from the top of the stack and store it in the PC
+                self.pc = self.ram[self.sp]
+                self.sp += 1
+
+            # CMP registerA registerB -- Compare the values in two registers
+            # FL bits: 00000LGE
+            elif IR == 0b10100111:
+                if self.reg[operand_a] == self.reg[operand_b]:
+                    self.fl = 0b00000001
+                elif self.reg[operand_a] > self.reg[operand_b]:
+                    self.fl = 0b00000010
+                else:
+                    self.fl = 0b00000100
+                self.pc += 3
+
+           # sprint challenge
+
+            # JMP register
+            elif IR == 0b01010100:
+                # Jump to the address stored in the given register; set pc to stored address
+
+                self.pc = self.reg[operand_a]
+
+            # JEQ register -- If equal flag is set (true), jump to the address stored
+            elif IR == 0b01010101:
+                # use bin() here so self.fl isn't passed as a decimal
+                if bin(self.fl)[-1] == '1':
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+
+            # JNE register -- If E flag is (false, 0), jump to the address stored
+            elif IR == 0b01010110:
+                if bin(self.fl)[-1] == '0':
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+
+            # ALU OP
+
+            # ADD + the value in two registers and store the result in registerA.
+            elif IR == 0b10100000:
+                self.alu('ADD', operand_a, operand_b)
+                self.pc += 3
+
+            # MUL * the values in two registers together and store the result in registerA.
+            elif IR == 0b10100010:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+
+            # MOD
+            elif IR == 0b10100100:
+                if self.reg[operand_b] != 0:
+                    self.alu("MOD", operand_a, operand_b)
+                    self.pc += 3
+                else:
+                    print("Error with R[b] in MOD")
+                    running = False
+
+            # AND
+            elif IR == 0b10101000:
+                self.alu("AND", operand_a, operand_b)
+                self.pc += 3
+            # OR
+            elif IR == 0b10101010:
+                self.alu("OR", operand_a, operand_b)
+                self.pc += 3
+
+            # XOR - XOR registerA registerB - Perform a bitwise-XOR between the values in registerA and registerB, storing the result in registerA.
+            elif IR == 0b10101011:
+                self.alu('XOR', operand_a, operand_b)
+                self.pc += 3
+
+            # NOT
+            elif IR == 0b01101001:
+                self.alu("NOT", operand_a, operand_b)
+                self.pc += 2
+
+            # SHL
+            elif IR == 0b10101100:
+                self.alu("SHL", operand_a, operand_b)
+                self.pc += 3
+            # SHR
+            elif IR == 0b10101101:
+                self.alu("SHR", operand_a, operand_b)
+                self.pc += 3
